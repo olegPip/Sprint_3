c44ff62dea0c761a89e7167988f23bf11cba9a5b
cybulin.oleg <olegpipulin@mail.ru>
2026-07-12 15:35:12 +0300

    the hetaera name_items and number_items were written

--- task_1.py
+++ task_1.py
@@ -1,11 +1,20 @@
 import datetime
 
-class OnlineSalesRegisterCollector:
+class OnlineSalesRegisterCollector: # Создан класс для работы с онлайн-кассой.   
 
     def __init__(self):
-        self.__name_items = []
-        self.__number_items = 0
-        self.__item_price = {'чипсы': 50, 'кола': 100, 'печенье': 45, 'молоко': 55, 'кефир': 70}
-        self.__tax_rate = {'чипсы': 20, 'кола': 20, 'печенье': 20, 'молоко': 10, 'кефир': 10}
+        self.__name_items = []     # Класс содержет, список name_items с перечнем товаров в чеке.
+        self.__number_items = 0    # Класс содержет, переменную number_items с количеством товаров в чеке.
+        self.__item_price = {'чипсы': 50, 'кола': 100, 'печенье': 45, 'молоко': 55, 'кефир': 70}  # Класс содержет, словарь item_price, где перечислены товары магазина и их стоимость.
+        self.__tax_rate = {'чипсы': 20, 'кола': 20, 'печенье': 20, 'молоко': 10, 'кефир': 10}     # Класс содержет, словарь tax_rate, где записана налоговая ставка на товары. Она составляет 10% или 20% от стоимости.  
 
-    ...# допиши код здесь
+    # Геттеры, которые получают значения name_items и number_items.  
+    # Геттер для получения списка названия товаров.
+    @property
+    def name_items(self):
+        return self.__name_items
+    
+    # Геттер для получения количества товаров
+    @property
+    def number_items(self):
+        return self.__number_items
